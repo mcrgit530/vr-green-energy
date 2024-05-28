8c141aeb0cc921f3156ecc35a25fd7d52424db35
MULE CHENNAKESAVA REDDY <164854745+mcrgit530@users.noreply.github.com>
2024-05-28 17:23:40 +0530

    Update Section.js

--- src/Section.js
+++ src/Section.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Section = ({ id, title, children }) => {
   return (
-    <div id={id} className="py-16 px-8">
+    <div id={id} className="py-32 px-8">
       <h2 className="text-3xl font-bold mb-4">{title}</h2>
       <div className="section-content">
         {children}
